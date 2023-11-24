657a380edde72b03fd127207c24a89dcef9765c7
nknikhil2013 <114844262+nknikhil2013@users.noreply.github.com>
2023-11-24 19:39:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const port = 5000;
 // Middleware to parse JSON
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // You can also use '*' to allow requests from any origin, but it's less secure:
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Use the car and booking routes
 app.use('/api/cars', carsRoute);
 app.use('/api/bookings', bookingsRoute);
